686d0541c4f9b7017f08b5d123fc041cb41e1e2f
Christina Everdyke <everdyke@uw.edu>
2023-09-05 15:16:00 -0700

    test

--- docsource/conf.py
+++ docsource/conf.py
@@ -42,8 +42,9 @@ extensions = [
     'sphinx.ext.graphviz',
     'sphinx.ext.autosectionlabel',
     'sphinx_tabs.tabs',
-    'sphinx.ext.apidoc',
+    'autoapi.extension'
 ]
+autoapi_dirs = ['../.']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
