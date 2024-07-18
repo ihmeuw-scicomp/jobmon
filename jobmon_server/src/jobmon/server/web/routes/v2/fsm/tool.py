98d456f756dda4d1b62057e0ad0e1a9d97179d77
Christina Everdyke <everdyke@uw.edu>
2024-07-18 15:37:21 -0700

    Fix lint

--- jobmon_server/src/jobmon/server/web/routes/v2/fsm/tool.py
+++ jobmon_server/src/jobmon/server/web/routes/v2/fsm/tool.py
@@ -1,9 +1,9 @@
 """Routes for Tools."""
 
+from datetime import datetime, timedelta
 from http import HTTPStatus as StatusCodes
 from typing import Any, cast, Dict
 
-from datetime import datetime, timedelta
 from flask import jsonify, request
 import sqlalchemy
 from sqlalchemy import select
@@ -82,28 +82,43 @@ def get_tool_versions(tool_id: int) -> Any:
     return resp
 
 
-def validate_date(date_str):
-    """ Validate if the date string is in 'YYYY-MM-DD' format. """
-    try:
-        datetime.strptime(date_str, '%Y-%m-%d')
-        return True
-    except ValueError:
-        return False
-
-
 @api_v1_blueprint.route("/tool/<tool_name>/tool_resource_usage", methods=["GET"])
 @api_v2_blueprint.route("/tool/<tool_name>/tool_resource_usage", methods=["GET"])
 def get_tool_resource_usage(tool_name: str) -> Any:
-    """
-    Returns requested and utilized resource usage and node args for every task instance associated with a given tool.
+    """Gets resource usage and node args for all TaskInstances associated with a given tool.
 
-    We limit this by date to not overwhelm the database.
+    We limit this to one week time spans to not overwhelm the database.
 
-    Parameters:
-        tool_name: name of the tool
+    Args:
+        tool_name (str): Name of the tool.
+        start_date (str): The start date in 'YYYY-MM-DD' format (query parameter).
+        end_date (str): The end date in 'YYYY-MM-DD' format (query parameter).
 
     Returns:
-        A list ?
+        List[Dict[str, Any]]: A list of dictionaries containing TaskInstance ID,
+        node argument value, TaskInstance maxrss, TaskInstance wallclock, and
+        requested resources.
+
+    Example Call:
+        /tool/large_wf_tool/tool_resource_usage?start_date=2024-07-11&end_date=2024-07-18
+
+    Example Response:
+        [
+            {
+                "node_arg_val": "--provenance True",
+                "ti_id": 12345677,
+                "ti_maxrss": 50844672,
+                "ti_requested_resources": {"runtime": 21600, "memory": 10},
+                "ti_wallclock": 20
+            },
+            {
+                "node_arg_val": "--intrinsic False",
+                "ti_id": 12345678,
+                "ti_maxrss": 43960320,
+                "ti_requested_resources": {"runtime": 21600, "memory": 10},
+                "ti_wallclock": 22
+            }
+        ]
     """
     arguments = request.args
     start_date = arguments.get("start_date")
@@ -111,25 +126,32 @@ def get_tool_resource_usage(tool_name: str) -> Any:
 
     # Validate that user passed in both dates
     if not start_date or not end_date:
-        return jsonify({
-            'error': 'Both start_date and end_date are required.'
-        }), StatusCodes.BAD_REQUEST
+        return (
+            jsonify({"error": "Both start_date and end_date are required."}),
+            StatusCodes.BAD_REQUEST,
+        )
 
     try:
-        start_date_object = datetime.strptime(start_date, '%Y-%m-%d').date()
-        end_date_object = datetime.strptime(end_date, '%Y-%m-%d').date()
+        start_date_object = datetime.strptime(start_date, "%Y-%m-%d").date()
+        end_date_object = datetime.strptime(end_date, "%Y-%m-%d").date()
     except ValueError:
-        return jsonify({
-            'error': 'Dates must be in the format YYYY-MM-DD.'
-        }), StatusCodes.BAD_REQUEST
+        return (
+            jsonify({"error": "Dates must be in the format YYYY-MM-DD."}),
+            StatusCodes.BAD_REQUEST,
+        )
 
     date_difference = end_date_object - start_date_object
 
     # Check if the difference exceeds one week
     if date_difference > timedelta(weeks=1):
-        return jsonify({
-            'error': 'The difference between start_date and end_date cannot exceed one week.'
-        }), StatusCodes.BAD_REQUEST
+        return (
+            jsonify(
+                {
+                    "error": "Time between start_date and end_date cannot exceed one week."
+                }
+            ),
+            StatusCodes.BAD_REQUEST,
+        )
 
     session = SessionLocal()
     with session.begin():
@@ -140,41 +162,20 @@ def get_tool_resource_usage(tool_name: str) -> Any:
                 TaskInstance.maxrss,
                 NodeArg.val,
                 TaskResources.requested_resources,
-            ).join_from(
-                TaskInstance,
-                Task,
-                TaskInstance.task_id == Task.id
             )
+            .join_from(TaskInstance, Task, TaskInstance.task_id == Task.id)
+            .join_from(Task, Workflow, Task.workflow_id == Workflow.id)
             .join_from(
-                Task,
-                Workflow,
-                Task.workflow_id == Workflow.id
+                Workflow, ToolVersion, Workflow.tool_version_id == ToolVersion.id
             )
-            .join_from(
-                Workflow,
-                ToolVersion,
-                Workflow.tool_version_id == ToolVersion.id
-            )
-            .join_from(
-                ToolVersion,
-                Tool,
-                ToolVersion.tool_id == Tool.id
-            )
-            .join_from(
-                Task,
-                NodeArg,
-                Task.node_id == NodeArg.node_id
-            )
-            .join_from(
-                Task,
-                TaskResources,
-                Task.task_resources_id == TaskResources.id
-            )
+            .join_from(ToolVersion, Tool, ToolVersion.tool_id == Tool.id)
+            .join_from(Task, NodeArg, Task.node_id == NodeArg.node_id)
+            .join_from(Task, TaskResources, Task.task_resources_id == TaskResources.id)
             .where(
                 Tool.name == tool_name,
                 Workflow.created_date.between(start_date, end_date),
-                Task.status == 'D',
-                TaskInstance.status == 'D'
+                Task.status == "D",
+                TaskInstance.status == "D",
             )
         )
         results = session.execute(sql).all()
@@ -184,7 +185,7 @@ def get_tool_resource_usage(tool_name: str) -> Any:
         "ti_wallclock",
         "ti_maxrss",
         "node_arg_val",
-        "ti_requested_resources"
+        "ti_requested_resources",
     )
     results = [dict(zip(column_names, result)) for result in results]
 
