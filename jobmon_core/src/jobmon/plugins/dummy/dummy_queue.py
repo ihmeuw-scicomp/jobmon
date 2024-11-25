5ed8204b66e2d5150f54dbac0abe68473073771b
limingxu <limingxu@uw.edu>
2024-11-25 15:03:41 -0800

    dummy cluster

---
+++ jobmon_core/src/jobmon/plugins/dummy/dummy_queue.py
@@ -0,0 +1,47 @@
+"""The client for the Dummy Executor."""
+
+from typing import Dict, List, Tuple, Union
+
+from jobmon.core.cluster_protocol import ClusterQueue
+
+
+class DummyQueue(ClusterQueue):
+    """Implementation of the dummy executor queue, derived from ClusterQueue."""
+
+    def __init__(self, queue_id: int, queue_name: str, parameters: dict) -> None:
+        """Intialization of SequentialQueue.
+
+        Get the limits from database in client.
+        """
+        self._queue_id = queue_id
+        self._queue_name = queue_name
+        self._parameters = parameters
+
+    def validate_resources(
+        self, strict: bool = False, **kwargs: Union[str, int, float]
+    ) -> Tuple[bool, str]:
+        """No resources defined for dummy execution. All resources valid."""
+        return True, ""
+
+    def coerce_resources(self, **kwargs: Union[str, int, float]) -> Dict:
+        return kwargs
+
+    @property
+    def queue_id(self) -> int:
+        """Return the ID of the queue."""
+        return self._queue_id
+
+    @property
+    def queue_name(self) -> str:
+        """Return the name of the queue."""
+        return self._queue_name
+
+    @property
+    def parameters(self) -> Dict:
+        """Return the dictionary of parameters."""
+        return self._parameters
+
+    @property
+    def required_resources(self) -> List:
+        """No specified required resources for dummy, return empty list."""
+        return []
