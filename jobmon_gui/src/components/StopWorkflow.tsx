d6cd0ea27cc1412bb6bea40c57fe2e439a9d31d3
Christina Everdyke <everdyke@uw.edu>
2024-12-13 15:36:54 -0800

    Fix typing errors

--- jobmon_gui/src/components/StopWorkflow.tsx
+++ jobmon_gui/src/components/StopWorkflow.tsx
@@ -14,6 +14,7 @@ import {jobmonAxiosConfig} from "@jobmon_gui/configs/Axios.ts";
 
 type StopWorkflowButtonProps = {
     wf_id: number | string
+    disabled: boolean
 }
 
 export default function StopWorkflowButton({
