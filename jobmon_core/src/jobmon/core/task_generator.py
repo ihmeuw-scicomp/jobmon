f8b68852baf52763bef9e7b1aa3a198e6307e44a
Leo Zoeckler <lzoeckle@uw.edu>
2025-04-02 14:53:40 -0700

    Revert "adding upstreams and updating tests for task generators"
    This reverts commit 460653224858267966d62c157c05ca55bfab84ad.

--- jobmon_core/src/jobmon/core/task_generator.py
+++ jobmon_core/src/jobmon/core/task_generator.py
@@ -662,7 +662,6 @@ class TaskGenerator:
         cluster_name: str = "",
         compute_resources: Optional[Dict] = None,
         resource_scales: Optional[Dict[str, Any]] = None,
-        upstream_tasks: List[Task] = [],
         **kwargs: Any,
     ) -> Task:
         """Create a task for the task_function with the given kwargs."""
@@ -709,7 +708,6 @@ class TaskGenerator:
             resource_scales=resource_scales,
             max_attempts=self.max_attempts,
             executable=executable_path,
-            upstream_tasks=upstream_tasks,
             **kwargs_for_task,  # type: ignore
         )
 
@@ -720,7 +718,6 @@ class TaskGenerator:
         cluster_name: str = "",
         compute_resources: Optional[Dict] = None,
         resource_scales: Optional[Dict[str, Any]] = None,
-        upstream_tasks: Optional[List[Task]] = None,
         **kwargs: Any,
     ) -> List[Task]:
         """Create a task array for the task_function with the given kwargs."""
@@ -758,7 +755,6 @@ class TaskGenerator:
             resource_scales=resource_scales,
             max_attempts=self.max_attempts,
             executable=executable_path,
-            upstream_tasks=upstream_tasks,
             **kwargs_for_task,  # type: ignore
         )
 
